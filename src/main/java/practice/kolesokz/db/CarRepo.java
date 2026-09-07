912fff3679cd37ccf9764efe34b91dfce249d83a
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-07 13:23:00 +0500

    refactor: enable specification based car filtering

--- src/main/java/practice/kolesokz/db/CarRepo.java
+++ src/main/java/practice/kolesokz/db/CarRepo.java
@@ -1,20 +1,10 @@
 package practice.kolesokz.db;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import practice.kolesokz.Entities.Car;
-import practice.kolesokz.Entities.CarStatus;
-
-import java.math.BigDecimal;
 
 @Repository
-public interface CarRepo extends JpaRepository<Car, Long> {
-
-    Page<Car> findByNameContainingIgnoreCaseAndStatus(String name, CarStatus status, Pageable pageable);
-
-    Page<Car> findByPriceBetweenAndStatus(BigDecimal minPrice, BigDecimal maxPrice, CarStatus status, Pageable pageable);
-
-    Page<Car> findByCountryIdAndStatus(Long countryId, CarStatus status, Pageable pageable);
+public interface CarRepo extends JpaRepository<Car, Long>, JpaSpecificationExecutor<Car> {
 }
