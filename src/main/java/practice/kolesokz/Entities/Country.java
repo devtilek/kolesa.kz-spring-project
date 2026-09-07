550cd1aba1feeea4b04127b7d90eaf4ad21bce69
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-07 13:12:13 +0500

    refactor: strengthen country entity constraints

--- src/main/java/practice/kolesokz/Entities/Country.java
+++ src/main/java/practice/kolesokz/Entities/Country.java
@@ -1,6 +1,8 @@
 package practice.kolesokz.Entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,18 +11,20 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-@Table(name = "t_country")
+@Table(name = "countries")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Country {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column
     private Long id;
 
-    @Column
+    @NotBlank
+    @Size(max = 100)
+    @Column(nullable = false, unique = true, length = 100)
     private String name;
 
-    @Column
+    @Size(max = 1000)
+    @Column(length = 1000)
     private String url;
 }
