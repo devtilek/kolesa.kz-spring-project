fa30480effe05aad3573d355c7569b016d91c2c7
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-07 13:23:04 +0500

    feat: add car search service contract

--- src/main/java/practice/kolesokz/services/CarService.java
+++ src/main/java/practice/kolesokz/services/CarService.java
@@ -1,11 +1,16 @@
 package practice.kolesokz.services;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import practice.kolesokz.Entities.Car;
+import practice.kolesokz.Entities.CarStatus;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface CarService {
     List<Car> findAll();
+    Page<Car> search(String name, BigDecimal minPrice, BigDecimal maxPrice, Long countryId, CarStatus status, Pageable pageable);
     Car findById(Long id);
     Car create(Car car, Long countryId);
     Car update(Long id, Car car, Long countryId);
