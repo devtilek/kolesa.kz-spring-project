86bd343e30901641ed03fc7c6779f8c128128011
Aktilek Korganbek <kalabaevbakitjan@gmail.com>
2026-09-07 13:22:53 +0500

    feat: add pageable car search repository methods

--- src/main/java/practice/kolesokz/db/CarRepo.java
+++ src/main/java/practice/kolesokz/db/CarRepo.java
@@ -1,10 +1,20 @@
 package practice.kolesokz.db;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import practice.kolesokz.Entities.Car;
+import practice.kolesokz.Entities.CarStatus;
+
+import java.math.BigDecimal;
+
 @Repository
-@Transactional
 public interface CarRepo extends JpaRepository<Car, Long> {
+
+    Page<Car> findByNameContainingIgnoreCaseAndStatus(String name, CarStatus status, Pageable pageable);
+
+    Page<Car> findByPriceBetweenAndStatus(BigDecimal minPrice, BigDecimal maxPrice, CarStatus status, Pageable pageable);
+
+    Page<Car> findByCountryIdAndStatus(Long countryId, CarStatus status, Pageable pageable);
 }
